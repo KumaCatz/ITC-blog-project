737afbf06a20a991ade9b42413a156656a20d351
KumaCatz <32141751+KumaCatz@users.noreply.github.com>
2023-07-20 13:39:45 +0300

    finished M1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import tweetModel from './components/tweetModel';
 import CreateTweet from './components/CreateTweet';
 import TweetsList from './components/TweetsList';
 
 function App() {
+  let storedTweets = JSON.parse(localStorage.getItem('tweets'));
+  let storedId = JSON.parse(localStorage.getItem('id'));
+  if (storedTweets == null || storedId == null) {
+    storedTweets = [];
+    storedId = 0;
+  }
   const [body, setBody] = useState('');
-  const [id, setId] = useState(0);
-  const [tweets, setTweets] = useState([]);
+  const [id, setId] = useState(storedId);
+  const [tweets, setTweets] = useState(storedTweets);
+
+  function handleCache() {
+    localStorage.clear()
+  }
 
   function handleChange(e) {setBody(e.target.value)}
 
@@ -18,8 +28,14 @@ function App() {
     setId(id + 1);
   }
 
+  useEffect(() => {
+    localStorage.setItem('tweets', JSON.stringify(tweets))
+    localStorage.setItem('id', JSON.stringify(id))
+  }, [ tweets, id ])
+
   return (
     <div className="App">
+      <button onClick={ handleCache } >clear cache</button>
       <CreateTweet handleChange={ handleChange } handleGenerate={ handleGenerate } />
       <TweetsList tweets={ tweets } />
     </div>
--- src/components/TweetsList.jsx
+++ src/components/TweetsList.jsx
@@ -2,16 +2,16 @@ import '../css/TweetsList.css'
 
 function TweetsList({ tweets }) {
     const tweetList = tweets.map(tweet =>
-        <div key={ tweet.id }>
+        <div key={ tweet.id } className='tweet'>
             <header>
                 <div>{ tweet.username }</div>
                 <div>{ tweet.date }</div>
             </header>
             <div>{ tweet.body }</div>
         </div>
-    );
+    ).reverse();
 
-    return <div >{ tweetList }</div>;
+    return <div>{ tweetList }</div>;
 }
 
 export default TweetsList
