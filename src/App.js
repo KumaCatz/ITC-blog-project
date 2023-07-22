cecb8914fe7ac710bbafd94d787048b105711980
KumaCatz <tod.kagan@gmail.com>
2023-07-23 02:32:44 +0300

    M3 done(almost)

--- src/App.js
+++ src/App.js
@@ -1,56 +1,22 @@
-import { React, useEffect, useState } from 'react';
-import fetchTweet from './components/fetchTweet';
-import tweetModel from './components/tweetModel';
-import CreateTweet from './components/CreateTweet';
-import Loading from './components/Loading';
-import TweetsList from './components/TweetsList';
+import { React } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Profile from './components/Profile';
+
+
 import './App.css';
 
 function App() {
-  const [body, setBody] = useState('');
-  const [tweets, setTweets] = useState([]);
-  const [disabled, setDisabled] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch('https://64b90fb679b7c9def6c0853b.mockapi.io/tweet');
-        const data = await response.json();
-        setTweets(data);
-        setLoading(false);
-      } catch(e) {
-        console.log(e)
-      }
-    }
-    fetchData();
-  }, [])
-
-  function handleChange(e) {
-    setBody(e.target.value)
-    if(e.target.value.length == 140) {
-      setDisabled(true)
-    } else {
-      setDisabled(false)
-    }
-  }
-
-  async function handleGenerate(e) {
-    e.preventDefault();
-    if (body == '') {return}
-    setLoading(true);
-    const newTweet = await fetchTweet(tweetModel('KumaCat', body));
-    console.log(newTweet);
-
-    setTweets([...tweets, newTweet]);
-    setLoading(false);
-  }
 
   return (
     <div className="App">
-      <CreateTweet handleChange={ handleChange } disabled={ disabled } handleGenerate={ handleGenerate } />
-      {loading ? <Loading /> : null}
-      <TweetsList tweets={ tweets } />
+      <Routes>
+          <Route path='/' element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path='profile' element={<Profile />} />
+          </Route>
+      </Routes>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,57 @@
-const Home = () => {
+import { React, useState, useEffect } from 'react';
+import CreateTweet from './CreateTweet';
+import Loading from './Loading';
+import TweetsList from './TweetsList';
+import fetchTweet from './fetchTweet';
+import tweetModel from './tweetModel';
 
+import '../css/Home.css'
+
+const Home = () => {
+    const [disabled, setDisabled] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [body, setBody] = useState('');
+    const [tweets, setTweets] = useState([]);
+  
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch('https://64b90fb679b7c9def6c0853b.mockapi.io/tweet');
+                const data = await response.json();
+                setTweets(data);
+                setLoading(false);
+            } catch(e) {
+                console.log(e)
+            }};
+        fetchData();
+    }, [])
+
+    function handleChange(e) {
+        setBody(e.target.value);
+        if(e.target.value.length == 140) {
+            setDisabled(true)
+        } else {
+            setDisabled(false)
+        }
+    }
+    
+    async function handleGenerate(e) {
+        e.preventDefault();
+        if (body == '') {return}
+        setLoading(true);
+        const newTweet = await fetchTweet(tweetModel('KumaCat', body));
+
+        setTweets([...tweets, newTweet]);
+        setLoading(false);
+    }
+
+    return (
+        <div className="Home">
+            <CreateTweet handleChange={ handleChange } disabled={ disabled } handleGenerate={ handleGenerate } />
+            {loading ? <Loading /> : null}
+            <TweetsList tweets={ tweets } />
+        </div>
+    )
 }
 
 export default Home
